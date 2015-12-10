9bad5141ac3bd5a5c577136a6d51abb136527cf5
Amit Miller <Amit Miller>
2015-12-10 11:53:32 +0200

    added support in device motion sensor (v1)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,40 +1,6 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $cordovaDeviceMotion) {
-
-  $scope.test = 0;
-
-  document.addEventListener("deviceready", function () {
-
-    $scope.test = 1;
-
-    $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
-      $scope.accelerationResult = result;
-      $scope.test = 2;
-    }, function(err) {
-      $scope.test = 'err';
-      // An error occurred. Show a message to the user
-    });
-
-    var options = { frequency: 2000 };
-
-    var watch = $cordovaDeviceMotion.watchAcceleration(options);
-    watch.then(
-      null,
-      function(error) {
-        // An error occurred
-      },
-      function(result) {
-        var X = result.x;
-        var Y = result.y;
-        var Z = result.z;
-        var timeStamp = result.timestamp;
-
-        $scope.test = Math.random() + " " + result;
-
-      });
-
-  }, false);
  })
 
 .controller('ChatsCtrl', function($scope, Chats) {
